5a68e27bfb87d5ac748c5b26ca28c3360c45fe67
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2025-12-09 16:38:38 +0700

    Create repository for RefreshToken

---
+++ src/main/java/com/backend/GoPlay/repository/RefreshTokenRepository.java
@@ -0,0 +1,20 @@
+package com.backend.GoPlay.repository;
+
+import com.backend.GoPlay.model.RefreshToken;
+import com.backend.GoPlay.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    int deleteByUser(User user);
+}
