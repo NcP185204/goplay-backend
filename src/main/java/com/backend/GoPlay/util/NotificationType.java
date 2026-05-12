de379a6e5a27334c90e9a652a1365d8eba61abf3
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-12 11:30:00 +0700

    Created the `NotificationType` enum to define and categorize different notification events (`SYSTEM`, `BOOKING_SUCCESS`, `BOOKING_CANCELLED`, `PAYMENT_FAILED`).

---
+++ src/main/java/com/backend/GoPlay/util/NotificationType.java
@@ -0,0 +1,8 @@
+package com.backend.GoPlay.util;
+
+public enum NotificationType {
+    SYSTEM,          // Thông báo hệ thống
+    BOOKING_SUCCESS, // Đặt sân thành công
+    BOOKING_CANCELLED,// Hủy đặt sân
+    PAYMENT_FAILED   // Thanh toán thất bại
+}
