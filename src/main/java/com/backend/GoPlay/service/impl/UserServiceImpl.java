7d4123273e74ca0afe664c35ca830aad6b83715b
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-12 11:38:58 +0700

    Annotated `UserServiceImpl` with `@Service` and `@RequiredArgsConstructor`, and made it implement `UserService`. ◦ Injected `UserRepository`. ◦ Implemented the `updateFcmToken` method to update and save the user's FCM token.

--- src/main/java/com/backend/GoPlay/service/impl/UserServiceImpl.java
+++ src/main/java/com/backend/GoPlay/service/impl/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.backend.GoPlay.service.impl;
 
-public class UserServiceImpl {
+import com.backend.GoPlay.model.User;
+import com.backend.GoPlay.repository.UserRepository;
+import com.backend.GoPlay.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void updateFcmToken(User user, String fcmToken) {
+        user.setFcmToken(fcmToken);
+        userRepository.save(user);
+    }
 }
