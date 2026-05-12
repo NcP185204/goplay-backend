7f836fa2b6cd34416359d37a381e3c76e4c73e9b
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-12 09:21:26 +0700

    Cleaned up formatting by removing unnecessary empty lines in the Court model. ◦ Removed an obsolete comment regarding the pricePerHour field.

--- src/main/java/com/backend/GoPlay/model/Court.java
+++ src/main/java/com/backend/GoPlay/model/Court.java
@@ -23,23 +23,17 @@ import java.util.Set;
         @Index(name = "idx_price", columnList = "pricePerHour")
 })
 public class Court {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
-
     @Column(nullable = false) private String name;
     @Column(nullable = false) private String address;
     @Column(columnDefinition = "TEXT") private String description;
-
     @Enumerated(EnumType.STRING)
     @Column(nullable = false) private SportType courtType;
-
-    // Trường này sẽ trở thành giá mặc định, hoặc có thể xóa đi
     private Double pricePerHour;
     private Double latitude;
     private Double longitude;
