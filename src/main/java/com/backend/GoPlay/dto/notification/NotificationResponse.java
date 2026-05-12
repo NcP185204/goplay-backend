bdafedaff16cdc53cde80c9584295cd5dbcb9db0
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-12 11:29:06 +0700

    Updated the `NotificationResponse` DTO to include properties for `id`, `title`, `content`, `type`, `relatedId`, `isRead`, and `createdAt`. ◦ Added Lombok `@Data` and `@Builder` annotations to streamline boilerplate code generation.

--- src/main/java/com/backend/GoPlay/dto/notification/NotificationResponse.java
+++ src/main/java/com/backend/GoPlay/dto/notification/NotificationResponse.java
@@ -1,4 +1,19 @@
 package com.backend.GoPlay.dto.notification;
 
+import com.backend.GoPlay.util.NotificationType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class NotificationResponse {
+    private Integer id;
+    private String title;
+    private String content;
+    private NotificationType type;
+    private Integer relatedId;
+    private boolean isRead;
+    private LocalDateTime createdAt;
 }
