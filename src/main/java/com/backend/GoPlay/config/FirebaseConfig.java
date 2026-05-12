75c2210e7e41b56ed86179497601f002d8243a26
Nguyễn Cao Phúc <nguyencaophuc.204@gmail.com>
2026-05-12 11:20:50 +0700

    Added FirebaseConfig to handle FirebaseApp initialization as a Spring Bean. ◦ Configured initialization to use GoogleCredentials loaded from the goplay-firebase-adminsdk.json classpath resource. ◦ Added a check to prevent re-initialization if a FirebaseApp instance already exists.

---
+++ src/main/java/com/backend/GoPlay/config/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package com.backend.GoPlay.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+
+        ClassPathResource resource = new ClassPathResource("goplay-firebase-adminsdk.json");
+        InputStream serviceAccount = resource.getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
+    }
+}
